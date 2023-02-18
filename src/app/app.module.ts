673a4cad2fdcbcb1fa993fc2580cb3bcc106286d
Aranxa Codina <tassa780@gmail.com>
2023-02-18 17:51:29 +0100

    CM-121: routing for home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { SnotifyModule, SnotifyService, ToastDefaults } from 'ng-snotify';
 import { AppComponent } from './app.component';
+import { Route, RouterModule } from '@angular/router';
 
+const routes: Route[] = [
+  {
+    path: '**',
+    loadChildren: () => import('./home/home.module').then((x) => x.HomeModule)
+  }
+]
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
-    imports: [
-        BrowserModule,
-    ],
-  providers: [],
+  imports: [
+    BrowserModule,
+    RouterModule,
+    SnotifyModule,
+    RouterModule.forRoot(routes),
+
+  ],
+  providers: [
+    { provide: 'SnotifyToastConfig', useValue: ToastDefaults },
+    SnotifyService,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
