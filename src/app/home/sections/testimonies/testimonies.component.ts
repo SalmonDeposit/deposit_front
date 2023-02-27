80d844319618e58cefa1b4496ae7c475721b0763
Aranxa Codina <tassa780@gmail.com>
2023-02-27 13:30:20 +0100

    wip

--- src/app/home/sections/testimonies/testimonies.component.ts
+++ src/app/home/sections/testimonies/testimonies.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import {BehaviorSubject} from "rxjs";
+
+import SwiperCore, { Keyboard, Pagination, Navigation, Virtual } from 'swiper';
+
+SwiperCore.use([Keyboard, Pagination, Navigation, Virtual]);
 
 @Component({
   selector: 'app-testimonies',
@@ -8,8 +13,12 @@ import { Component, OnInit } from '@angular/core';
 export class TestimoniesComponent implements OnInit {
 
   constructor() { }
+  slides$ = new BehaviorSubject<string[]>(['']);
 
   ngOnInit(): void {
+    this.slides$.next(
+      Array.from({ length: 600 }).map((el, index) => `Slide ${index + 1}`)
+    );
   }
 
 }
