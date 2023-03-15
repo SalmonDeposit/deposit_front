0f711e97c99aafd50321453e47e6b47b0096c1de
Aranxa Codina <tassa780@gmail.com>
2023-03-15 17:50:12 +0100

    CM-76 : manage document

--- src/app/dashboard/document-dashboard/document-dashboard.component.ts
+++ src/app/dashboard/document-dashboard/document-dashboard.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {DocumentService} from "../services/document.service";
 import {DepositDocument} from "../classes/models/document";
-import {faTrash} from "@fortawesome/free-solid-svg-icons";
+import {faTrash, faPlus} from "@fortawesome/free-solid-svg-icons";
 import {faFileCircleCheck} from "@fortawesome/free-solid-svg-icons";
 import {IconProp} from "@fortawesome/fontawesome-svg-core";
 import {Router} from "@angular/router";
@@ -12,10 +12,12 @@ import {Router} from "@angular/router";
   styleUrls: ['./document-dashboard.component.scss']
 })
 export class DocumentDashboardComponent implements OnInit{
+  addModal = false;
   documents?: DepositDocument[];
   isLoading = true;
   trash = faTrash as IconProp;
   file = faFileCircleCheck as IconProp;
+  plus = faPlus as IconProp;
   deleteModal = false;
   currentDeleteDocumentId?: string;
   constructor(public service: DocumentService, private router: Router) {
