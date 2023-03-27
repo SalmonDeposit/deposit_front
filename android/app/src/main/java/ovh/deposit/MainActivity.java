65e93bfeda8e09d001c41848fbf2d8277b386047
Aranxa Codina <tassa780@gmail.com>
2023-03-27 14:05:55 +0200

    CM-164 : add capacitor

---
+++ android/app/src/main/java/ovh/deposit/MainActivity.java
@@ -0,0 +1,5 @@
+package ovh.deposit;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ovh.deposit',
+  appName: 'Deposit',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+};
+
+export default config;
