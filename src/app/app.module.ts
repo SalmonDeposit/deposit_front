bccb54283b1275dff82f57096fe5831d0d01f950
Aranxa Codina <codinaaranxa@gmail.com>
2023-05-21 14:49:56 +0200

    CM-185: finish responsive on home page && add plans

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, LOCALE_ID } from '@angular/core';
 import { SnotifyModule, SnotifyService, ToastDefaults } from 'ng-snotify';
 import { AppComponent } from './app.component';
 import { Route, RouterModule } from '@angular/router';
@@ -12,7 +12,10 @@ import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
 import {UserGuard} from "../core/generics/guards/user.guard";
 import { PrivacyPolicyManagementComponent } from './privacy-policy-management/privacy-policy-management.component';
 import {FontAwesomeModule} from "@fortawesome/angular-fontawesome";
+import localeFr from '@angular/common/locales/fr';
+import {registerLocaleData} from "@angular/common";
 
+registerLocaleData(localeFr);
 const routes: Route[] = [
   {
     path: 'pages',
@@ -46,6 +49,7 @@ const routes: Route[] = [
     { provide: 'SnotifyToastConfig', useValue: ToastDefaults },
     { provide: HTTP_INTERCEPTORS, useClass: DepositTokenCrsfInterceptor, multi: true },
     { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    {provide: LOCALE_ID, useValue: 'fr-FR'},
     SnotifyService,
     {
       provide: 'env',
---
+++ src/app/home/classes/plan.ts
@@ -0,0 +1,7 @@
+export class Plan{
+  shortName: string;
+  longName: string;
+  salmonCoinCost: number;
+  description: string;
+  realCost:number
+}
