abc7324b53f1e7828ac308eb0274faa7e0327491
Aranxa Codina <codinaaranxa@gmail.com>
2023-05-23 16:29:06 +0200

    CM-205: fix tests

--- src/app/admin/contacts/contacts.component.spec.ts
+++ src/app/admin/contacts/contacts.component.spec.ts
@@ -1,16 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ContactsComponent } from './contacts.component';
+import {DEPOSIT_TEST_BED} from "../../../deposit.testbed";
+import {ContactComponent} from "../../pages/contact/contact.component";
+import {ContactFormBuilder} from "../../pages/contact/classes/contactFormBuilder";
 
 describe('ContactsComponent', () => {
   let component: ContactsComponent;
   let fixture: ComponentFixture<ContactsComponent>;
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ ContactsComponent ]
-    })
-    .compileComponents();
+    const tb = DEPOSIT_TEST_BED.forComponent(ContactsComponent);
+    await TestBed.configureTestingModule(tb)
+      .compileComponents();
 
     fixture = TestBed.createComponent(ContactsComponent);
     component = fixture.componentInstance;
